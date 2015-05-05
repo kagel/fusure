66b0308f5691c8883b3478cd7053429d8e712e63
dkhvatov <dimax4@gmail.com>
2015-05-05 20:02:19 +0800

    alchemyapi hello world

---
+++ src/cljs/fusure/services/alchemy.cljs
@@ -0,0 +1,31 @@
+(ns fusure.services.alchemy
+  (:import [goog.net Jsonp]
+           [goog Uri]))
+
+(def entities-endpoint "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities")
+
+(defn jsonp [uri & opts]
+  (let [{:keys [on-success on-timeout content callback-name callback-value timeout-ms]} opts
+        req        (goog.net.Jsonp. uri callback-name)
+        data       (when content (clj->js content))
+        on-success (when on-success #(on-success (js->clj % :keywordize-keys true)))
+        on-timeout (when on-timeout #(on-timeout (js->clj % :keywordize-keys true)))]
+    (when timeout-ms (.setRequestTimeout req timeout-ms))
+    (.log js/console (str "content: " data))
+    (.send req data on-success on-timeout callback-value)))
+
+(defn callback [data]
+  (.log js/console data))
+
+(defn entities [text]
+  (jsonp entities-endpoint
+         :content {:apikey             "b233d332e207ba07975d8eec9c8cf77340c74f21"
+                   :text               text
+                   :outputMode         "json"
+                   :structuredEntities "1"
+                   :jsonp              "fusure.services.alchemy.callback"}
+         :on-success (fn [data]
+                       (.log js/console data))
+         :on-error (fn [data]
+                     (.log js/console data)))
+  "")
