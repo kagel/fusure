5bdcbf7790d46fec5016da95581aa3f471f2b0ba
dkhvatov <dimax4@gmail.com>
2015-05-07 00:32:21 +0800

    alchemyapi sentiment hello world

--- src/cljs/fusure/services/alchemy.cljs
+++ src/cljs/fusure/services/alchemy.cljs
@@ -2,7 +2,10 @@
   (:import [goog.net Jsonp]
            [goog Uri]))
 
+(def non-searchable "b233d332e207ba07975d8eec9c8cf77340c74f21")
+
 (def entities-endpoint "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities")
+(def sentiment-endpoint "http://access.alchemyapi.com/calls/text/TextGetTextSentiment")
 
 (defn jsonp [uri & opts]
   (let [{:keys [on-success on-timeout content callback-name callback-value timeout-ms]} opts
@@ -19,7 +22,20 @@
 
 (defn entities [text]
   (jsonp entities-endpoint
-         :content {:apikey             "b233d332e207ba07975d8eec9c8cf77340c74f21"
+         :content {:apikey             non-searchable
+                   :text               text
+                   :outputMode         "json"
+                   :structuredEntities "1"
+                   :jsonp              "fusure.services.alchemy.callback"}
+         :on-success (fn [data]
+                       (.log js/console data))
+         :on-error (fn [data]
+                     (.log js/console data)))
+  "")
+
+(defn sentiments [text]
+  (jsonp sentiment-endpoint
+         :content {:apikey             non-searchable
                    :text               text
                    :outputMode         "json"
                    :structuredEntities "1"
